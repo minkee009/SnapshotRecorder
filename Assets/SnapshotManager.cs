8926c29d6e9e043cadefcad3ddb94c341817cc62
minkee009 <minkee5004@gmail.com>
2023-08-29 19:54:01 +0900

    초안 작성
    실험 몇 개 더 추가

--- Assets/SnapshotManager.cs
+++ Assets/SnapshotManager.cs
@@ -1,21 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 
 
 public class SnapshotManager : MonoBehaviour
 {
-    Queue<Snapshot> testQueue = new Queue<Snapshot>();
+    List<Snapshot> testQueue = new List<Snapshot>();
 
-    private void Start()
+    public Rigidbody rb;
+
+    public bool isRewind;
+
+    public float accumlator;
+
+    float _frameTime;
+
+    private void Update()
     {
+        if (isRewind)
+        {
+            return;
+        }
+
+        accumlator += Time.deltaTime;
+
+        if (accumlator > 1/5)
+        {
+            _frameTime += accumlator;
+            Debug.Log(_frameTime);
+            accumlator = 0f;
+            testQueue.Add(new Snapshot(rb.position, rb.rotation));
+            if(testQueue.Count > 50)
+            {
+                testQueue.Remove(testQueue[0]);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (testQueue.Count <= 0) return;
+
+        foreach(Snapshot s in testQueue)
+        {
+            Gizmos.DrawCube(s.position, Vector3.one * 2f);
+        }
         
     }
+
 }
 
 public struct Snapshot
 {
+    public Snapshot(Vector3 p, Quaternion r)
+    {
+        position = p;
+        rotation = r;
+    }
+
     public Vector3 position;
     public Quaternion rotation;
 }
