908489e0d105db27560a1e05f05caf231cfd5747
minkee009 <minkee5004@gmail.com>
2023-10-23 17:44:54 +0900

    recorder_01
    기본기능 제작

--- Assets/SnapshotManager.cs
+++ Assets/SnapshotManager.cs
@@ -7,59 +7,123 @@ using UnityEngine;
 
 public class SnapshotManager : MonoBehaviour
 {
-    List<Snapshot> testQueue = new List<Snapshot>();
+    List<Snapshot> snapshotBuffer = new List<Snapshot>();
 
     public Rigidbody rb;
 
+    public bool wasRewind;
     public bool isRewind;
 
     public float accumlator;
+    public float accumlatorTiming = 0.1f;
 
-    float _frameTime;
+    public float snapshotTimeLimit = 5f;
+
+    public Snapshot currentSnapshot = new Snapshot();
+    public float currentTiming;
+
+
+    private void Start()
+    {
+        snapshotBuffer.Add(new Snapshot(rb.position, rb.rotation, rb.velocity, rb.angularVelocity));
+    }
 
     private void Update()
+    {
+        wasRewind = isRewind;
+        isRewind = Input.GetKey(KeyCode.Space);
+        RecordSnapshot();
+        RewindSnapshot();
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (snapshotBuffer.Count <= 0) return;
+
+        foreach(Snapshot s in snapshotBuffer)
+        {
+            Gizmos.DrawCube(s.position, Vector3.one * 2f);
+        }
+    }
+
+    public void RecordSnapshot()
     {
         if (isRewind)
         {
             return;
         }
+        rb.isKinematic = false;
 
         accumlator += Time.deltaTime;
 
-        if (accumlator > 1/5)
+        if (accumlator > accumlatorTiming)
         {
-            _frameTime += accumlator;
-            Debug.Log(_frameTime);
             accumlator = 0f;
-            testQueue.Add(new Snapshot(rb.position, rb.rotation));
-            if(testQueue.Count > 50)
+            snapshotBuffer.Add(new Snapshot(rb.position, rb.rotation, rb.velocity,rb.angularVelocity));//, Time.time));
+            if (snapshotBuffer.Count * accumlatorTiming > snapshotTimeLimit)
             {
-                testQueue.Remove(testQueue[0]);
+                snapshotBuffer.RemoveAt(0);
             }
         }
     }
 
-    private void OnDrawGizmos()
+    public void RewindSnapshot()
     {
-        if (testQueue.Count <= 0) return;
-
-        foreach(Snapshot s in testQueue)
+        if (!isRewind || snapshotBuffer.Count - 1 <= 0)
         {
-            Gizmos.DrawCube(s.position, Vector3.one * 2f);
+            return;
         }
-        
-    }
+        rb.isKinematic = true;
 
+        if (isRewind && !wasRewind)
+        {
+            currentSnapshot = new Snapshot(rb.position, rb.rotation, rb.velocity, rb.angularVelocity);
+            currentTiming = accumlator;
+        }
+
+        accumlator -= Time.deltaTime;
+
+        if (Mathf.Approximately(0.0f, accumlator) || accumlator < 0)
+        {
+            accumlator = accumlatorTiming;
+            if (snapshotBuffer.Count - 1 <= 0)
+                return;
+            currentSnapshot = snapshotBuffer[snapshotBuffer.Count - 1];
+            snapshotBuffer.RemoveAt(snapshotBuffer.Count - 1);
+            currentTiming = accumlatorTiming;
+        }
+
+        var t = accumlator / currentTiming;
+
+        var rewindSnapshot = Snapshot.Lerp(snapshotBuffer[snapshotBuffer.Count - 1], currentSnapshot, t);
+
+        rb.position = rewindSnapshot.position;
+        rb.rotation = rewindSnapshot.rotation;
+    }
 }
 
 public struct Snapshot
 {
-    public Snapshot(Vector3 p, Quaternion r)
+    public Vector3 velocity;
+    public Vector3 angularVelocity;
+    public Vector3 position;
+    public Quaternion rotation;
+
+    public Snapshot(Vector3 p, Quaternion r, Vector3 v, Vector3 a)
     {
         position = p;
         rotation = r;
+        velocity = v;
+        angularVelocity = a;
     }
 
-    public Vector3 position;
-    public Quaternion rotation;
+    public static Snapshot Lerp(Snapshot pre, Snapshot next, float t)
+    {
+        var lerpVel = Vector3.Lerp(pre.velocity, next.velocity, t);
+        var lerpAngVel = Vector3.Lerp(pre.angularVelocity, next.angularVelocity, t);
+        var lerpPos = Vector3.Lerp(pre.position, next.position, t);
+        var lerpRot = Quaternion.Slerp(pre.rotation, next.rotation, t);
+
+        return new Snapshot(lerpPos, lerpRot, lerpVel, lerpAngVel);
+    }
 }
